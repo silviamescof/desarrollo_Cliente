f5a7a36a4023e1a83ae8f30672a9c78c4e0b066f
francis65000 <145580354+francis65000@users.noreply.github.com>
2023-10-28 20:29:59 +0200

    Insertar get, set, toString

--- prueba_final_electricidad/company.js
+++ prueba_final_electricidad/company.js
@@ -10,10 +10,49 @@ export class Company{
     };
 
     ////////setters////////
+    setNombre(nombre){
+        this.nombre = nombre;
+    }
 
-        
+    setResponsable(responsable){
+        this.responsable = responsable;
+    }
+
+    setCif(cif){
+        this.cif = cif;
+    }
+
+    setTelefono(telefono){
+        this.telefono = telefono;
+    }
+
+    setDireccion(direccion){
+        this.direccion = direccion;
+    }
+
     ////////getters///////
+    getNombre(){
+        return this.nombre;
+    }
 
+    getResponsable(){
+        return this.responsable;
+    }
+
+    getCif(){
+        return this.cif;
+    }
+
+    getTelefono(){
+        return this.telefono;
+    }
+
+    getDireccion(){
+        return this.direccion;
+    }
+    
     ///////toString///////
-
+    toString() {
+        return `Nombre: ${this.nombre}\nResponsable: ${this.responsable}\nCIF: ${this.cif}\nTeléfono: ${this.telefono}\nDirección: ${this.direccion}`;
+    }
 };
--- prueba_final_electricidad/school.js
+++ prueba_final_electricidad/school.js
@@ -14,15 +14,59 @@ export class School{
 
     ///////////setters//////////
 
+    set setNombre(nombre){
+        this.nombre=nombre;
+    };
+
+    set setLocalidad(localidad){
+        this.localidad=localidad;
+    };
+
+    set setResponsable(responsable){
+        this.responsable=responsable;
+    };
+
+    set setTelefono(telefono){
+        this.telefono=telefono;
+    };
+
+    set setDireccion(direccion){
+        this.direccion=direccion;
+    };
+
     set setInstalacion(instalacion){
         this.instalacion=instalacion instanceof Installation ? instalacion : null;
     };
-
+    
     ///////////getters///////////
 
     get getInstalacion(){
         return this.instalacion;
     };
 
+    get getNombre(){
+        return this.nombre;
+    };
+
+    get getLocalidad(){
+        return this.localidad;
+    };
+
+    get getResponsable(){
+        return this.responsable;
+    };
+
+    get getTelefono(){
+        return this.telefono;
+    };
+
+    get getDireccion(){
+        return this.direccion;
+    };
+
     ///////////toString///////////
+
+    toString() {
+        return `Nombre: ${this.nombre}\nLocalidad: ${this.localidad}\nResponsable: ${this.responsable}\nTeléfono: ${this.telefono}\nDirección: ${this.direccion}\nInstalación: ${this.instalacion ? this.instalacion.toString() : "No hay instalación asignada"}`;
+    }
 };
